e23adf9fb5aad82d86adafcfdeffeb1f4817ff37
Prakhar Trivedi <prakhar0706@gmail.com>
2021-03-15 22:41:44 +0800

    Installed instatus.ts package
    Will experiment since the documentation is heavily incomplete

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const Discord = require('discord.js')
-const client = new Discord.Client();
+const bot = new Discord.Client();
 require('dotenv').config();
+const { InstatusClient } = require('instatus.ts')
+const client = new InstatusClient({
+    key: `${process.env.INSTATUS_API_KEY}`
+})
 
 const token = process.env.DISCORD_TOKEN
 const PREFIX = process.env.BOT_PREFIX
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('Instatus Updater Bot is online!')
 })
 
-client.on('message', msg => {
+bot.on('message', msg => {
     if (!msg.content.startsWith(PREFIX)) return
     if (msg.guild.id != '805723501544603658') return msg.reply('Invalid Guild. This bot only works in the backend guild.')
     let args = msg.content.substring(PREFIX.length).split(' ')
     
 })
 
-client.login(token)
+bot.login(token)
